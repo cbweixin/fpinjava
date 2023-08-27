5f41e7c81ababbbe43d2a9e01df6ab787e013a8e
cbweixin <cbweixin@qq.com>
2023-08-26 17:59:33 -0700

    add Optional implmenation

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/test/java/com/fpinjava/functions/monad/MOptionalTest.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/test/java/com/fpinjava/functions/monad/MOptionalTest.java
@@ -73,6 +73,8 @@ public class MOptionalTest {
     Account acct;
     Account acct2;
 
+    Account acct3;
+
     @Before
     public void setUp() throws Exception {
         final City puqi = new City("puqi");
@@ -81,7 +83,10 @@ public class MOptionalTest {
 
         final City nullCity = new City();
         final Address address2 = new Address(nullCity);
-        acct2 = new Account(address);
+        acct2 = new Account(address2);
+
+        final Address address3 = new Address();
+        acct3 = new Account(address3);
     }
 
     @Test
@@ -95,6 +100,7 @@ public class MOptionalTest {
 
     }
 
+    @Test
     public void testMapWithNullCity() {
         String name = new MOptional<>(acct2).map(Account::getAddress)
                                             .map(Address::getCity)
@@ -102,5 +108,16 @@ public class MOptionalTest {
                                             .OrElse("Unknown");
         assertEquals("Unknown", name);
     }
+
+    public void testMapWithNullAddr() {
+        String name = new MOptional<>(acct3).map(Account::getAddress)
+                                            .map(Address::getCity)
+                                            .map(City::getName)
+                                            .OrElse("Unknown");
+        assertEquals("Unknown", name);
+
+    }
+
+
 
 }
